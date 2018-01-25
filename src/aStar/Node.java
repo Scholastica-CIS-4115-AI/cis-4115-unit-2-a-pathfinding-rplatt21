5a94d62d3ade0978e06b9f3c3015ae758993309e
tgibbons <tgibbons@143.110.33.51>
2018-01-25 09:39:46 -0600

    Added comments in JavaDoc format

--- src/aStar/Node.java
+++ src/aStar/Node.java
@@ -3,106 +3,129 @@ package aStar;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * The AreaMap class defines a graph representing a map of Duluth, MN.
+ * Implements Comparable interface to allow sorting, requires compareTo() method
+ *
+ * This does not need to be modified by students
+ *
+ * @author Original author: Tom Gibbons.
+ * @version Spring 2018 version
+ */
 public class Node implements Comparable<Node> {
-        /* Nodes that this is connected to */
-        AreaMap map;
-        ArrayList<Node> neighborList;				// list of neighbor nodes to this node
-        ArrayList<Double> neighborDistance;			// distance to each of the neighbor nodes in above list
-        boolean visited;
-        public double distanceFromStart;			// distance from Start node to this node
-        public double heuristicDistanceToGoal;			// estimated distance from node to Goal node
-        public double TotalDistanceFromGoal;			// estimated distance from start to Goal node going through this node, TotalDistanceFromGoal = heuristicDistanceToGoal + distanceFromStart
-        Node previousNode;					// previous node in the path to this node.  How did we get to this node.
-        public int nodeNum;					// the number of this node on the map
-        public double x;					// x coordinate of this node
-        public double y;					// y coordinate of this node
-        boolean isStart;					// is this a start node?
-        boolean isGoal;						// is this a goal node?
-        
-        Node(int n, double x, double y) {
-                neighborList = new ArrayList<Node>();
-                neighborDistance = new ArrayList<Double>();
-                this.nodeNum = n;
-                this.x = x;
-                this.y = y;
-                this.visited = false;
-                this.distanceFromStart = Integer.MAX_VALUE;
-                this.isStart = false;
-                this.isGoal = false;
-        }
-        
-        Node (double x, double y, boolean visited, int distanceFromStart , boolean isStart, boolean isGoal) {
-                neighborList = new ArrayList<Node>();
-                this.x = x;
-                this.y = y;
-                this.visited = visited;
-                this.distanceFromStart = distanceFromStart;
-                this.isStart = isStart;
-                this.isGoal = isGoal;
-        }
-        
-        public Double getNeighborDistance(int i) {
-            return neighborDistance.get(i);
-        }
-        
-        public int getNeighborSize() {
-            return neighborList.size();
-        }
-        
-        public Node getNeighbor(int i) {
-            return neighborList.get(i);
-        }
-        
-        public void setNeighbor(Node n, double dist) {
-        	neighborList.add(n);
-        	neighborDistance.add(dist);
-        }
 
-        public boolean isVisited() {
-                return visited;
-        }
+    AreaMap map;                                        // Map that this is a part of
+    ArrayList<Node> neighborList;                       // list of neighbor nodes to this node
+    ArrayList<Double> neighborDistance;			// distance to each of the neighbor nodes in above list
+    boolean visited;
+    public double distanceFromStart;			// distance from Start node to this node
+    public double heuristicDistanceToGoal;		// estimated distance from node to Goal node
+    public double TotalDistance;                        // estimated distance from start to Goal node going through this node, TotalDistanceFromGoal = heuristicDistanceToGoal + distanceFromStart
+    Node previousNode;					// previous node in the path to this node.  How did we get to this node.
+    public int nodeNum;					// the number of this node on the map
+    public double x;					// x coordinate of this node
+    public double y;					// y coordinate of this node
+    boolean isStart;					// is this a start node?
+    boolean isGoal;						// is this a goal node?
 
-        public void setVisited(boolean visited) {
-                this.visited = visited;
-        }
+    /**
+     * This constructor creates the node that is not a start or goal and sets distance from start to max
+     */
+    Node(int n, double x, double y) {
+        neighborList = new ArrayList<Node>();
+        neighborDistance = new ArrayList<Double>();
+        this.nodeNum = n;
+        this.x = x;
+        this.y = y;
+        this.visited = false;
+        this.distanceFromStart = Integer.MAX_VALUE;
+        this.isStart = false;
+        this.isGoal = false;
+    }
+    /**
+     * This constructor creates the node with all the given parameters
+     */
+    Node(double x, double y, boolean visited, int distanceFromStart, boolean isStart, boolean isGoal) {
+        neighborList = new ArrayList<Node>();
+        this.x = x;
+        this.y = y;
+        this.visited = visited;
+        this.distanceFromStart = distanceFromStart;
+        this.isStart = isStart;
+        this.isGoal = isGoal;
+    }
+    /**
+     * getNeighborDistance() returns the distance the the i'th neighbor
+     * @param i is the index for the neighbor. This is not checked for bounds
+     */
+    public Double getNeighborDistance(int i) {
+        return neighborDistance.get(i);
+    }
+    /**
+     * getNeighborSize() returns number of neighbors
+     */
+    public int getNeighborSize() {
+        return neighborList.size();
+    }
 
-        public Node getPreviousNode() {
-                return previousNode;
-        }
+    public Node getNeighbor(int i) {
+        return neighborList.get(i);
+    }
 
-        public void setPreviousNode(Node previousNode) {
-                this.previousNode = previousNode;
-        }
-              
-        public boolean isStart() {
-                return isStart;
-        }
+    public void setNeighbor(Node n, double dist) {
+        neighborList.add(n);
+        neighborDistance.add(dist);
+    }
 
-        public void setStart(boolean isStart) {
-                this.isStart = isStart;
-        }
+    public boolean isVisited() {
+        return visited;
+    }
 
-        public boolean isGoal() {
-                return isGoal;
-        }
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
 
-        public void setGoal(boolean isGoal) {
-                this.isGoal = isGoal;
-        }
+    public Node getPreviousNode() {
+        return previousNode;
+    }
 
-        public boolean equals(Node node) {
-                return (node.x == x) && (node.y == y);
-        }
+    public void setPreviousNode(Node previousNode) {
+        this.previousNode = previousNode;
+    }
 
-        // Compare this node with another node using TotalDistanceFromGoal.
-        //  Used by Collections.sort to sort the list in order by total distance
-        public int compareTo(Node otherNode) {               
-                if (TotalDistanceFromGoal < otherNode.TotalDistanceFromGoal) {
-                        return -1;
-                } else if (TotalDistanceFromGoal > otherNode.TotalDistanceFromGoal) {
-                        return 1;
-                } else {
-                        return 0;
-                }
+    public boolean isStart() {
+        return isStart;
+    }
+
+    public void setStart(boolean isStart) {
+        this.isStart = isStart;
+    }
+
+    public boolean isGoal() {
+        return isGoal;
+    }
+
+    public void setGoal(boolean isGoal) {
+        this.isGoal = isGoal;
+    }
+
+    public boolean equals(Node node) {
+        return (node.x == x) && (node.y == y);
+    }
+    /**
+     * compareTo() Compare this node with another node using TotalDistanceFromGoal.
+     *        Used by Collections.sort to sort the list in order by total distance
+     * @param otherNode is the node to compare this node to
+     * @return a int where -1 is when this node has less total distance, 
+     *                     +1 is when this node has larger total distance
+     */
+    public int compareTo(Node otherNode) {
+        if (TotalDistance < otherNode.TotalDistance) {
+            return -1;
+        } else if (TotalDistance > otherNode.TotalDistance) {
+            return 1;
+        } else {
+            return 0;
         }
+    }
 }
--- src/aStar/Path.java
+++ src/aStar/Path.java
@@ -2,90 +2,114 @@ package aStar;
 
 import java.util.ArrayList;
 
+/**
+ * The Path class defines an ordered list of nodes making up a path.
+ *
+ * This does not need to be modified by students
+ *
+ * @author Original author: Tom Gibbons.
+ * @version Spring 2018 version
+ */
 public class Path {
-        // The waypoints in the path (list of coordinates making up the path)
-        private ArrayList<Node> waypoints = new ArrayList<Node>();
-        
-        public Path() {
+    // The waypoints in the path (list of coordinates making up the path)
+
+    private ArrayList<Node> waypoints = new ArrayList<Node>();
+
+    /**
+     * default constructor
+     */
+    public Path() {
+    }
+
+    /**
+     * getLength returns the number of nodes on the path.
+     *
+     * @return number of nodes on the path as an integer.
+     */
+    public int getLength() {
+        return waypoints.size();
+    }
+    /**
+     * getWayPoint returns the i'th of nodes on the path defined by the index
+     *
+     * @return the nodes on the path
+     */
+    public Node getWayPoint(int index) {
+        return waypoints.get(index);
+    }
+
+    /**
+     * Get the x-coordinate for the waypoint at the given index.
+     *
+     * @param index The index of the waypoint to get the x-coordinate of.
+     * @return The x coordinate at the waypoint.
+     */
+    public double getX(int index) {
+        return getWayPoint(index).x;
+    }
+
+    /**
+     * Get the y-coordinate for the waypoint at the given index.
+     *
+     * @param index The index of the waypoint to get the y-coordinate of.
+     * @return The y coordinate at the waypoint.
+     */
+    public double getY(int index) {
+        return getWayPoint(index).y;
+    }
+
+    /**
+     * Append a waypoint to the path.
+     *
+     * @param x The x coordinate of the waypoint.
+     * @param y The y coordinate of the waypoint.
+     */
+    public void appendWayPoint(Node n) {
+        waypoints.add(n);
+    }
+
+    /**
+     * Add a waypoint to the beginning of the path.
+     *
+     * @param x The x coordinate of the waypoint.
+     * @param y The y coordinate of the waypoint.
+     */
+    public void prependWayPoint(Node n) {
+        waypoints.add(0, n);
+    }
+
+    /**
+     * contains() checks if this path contains the WayPoint
+     *
+     * @param x The x coordinate of the waypoint.
+     * @param y The y coordinate of the waypoint.
+     * @return True if the path contains the waypoint.
+     */
+    public boolean contains(int x, int y) {
+        for (Node node : waypoints) {
+            if (node.x == x && node.y == y) {
+                return true;
+            }
         }
-        
-        public int getLength() {
-                return waypoints.size();
+        return false;
+    }
+    /**
+     * printPath() prints the nodes on the path
+     */
+    public void printPath() {
+        Node n, prev_n;
+        int i = 0;
+
+        System.out.println("Nodes on path:");
+        n = waypoints.get(i);
+        System.out.println("Path " + n.nodeNum + " (" + n.x + " , " + n.y + ")");
+        for (i = 1; i < waypoints.size(); i++) {
+            prev_n = n;
+            n = waypoints.get(i);
+            System.out.println("Path " + n.nodeNum + " (" + n.x + " , " + n.y + ")");
+            //mc.drawLine(prev_n.getX(), prev_n.getY(), n.getX(), n.getY());
         }
 
-        public Node getWayPoint(int index) {
-                return waypoints.get(index);
-        }
-
-        /**
-         * Get the x-coordinate for the waypoint at the given index.
-         * 
-         * @param index The index of the waypoint to get the x-coordinate of.
-         * @return The x coordinate at the waypoint.
-         */
-        public double getX(int index) {
-                return getWayPoint(index).x;
-        }
-
-        /**
-         * Get the y-coordinate for the waypoint at the given index.
-         * 
-         * @param index The index of the waypoint to get the y-coordinate of.
-         * @return The y coordinate at the waypoint.
-         */
-        public double getY(int index) {
-                return getWayPoint(index).y;
-        }
-
-        /**
-         * Append a waypoint to the path.  
-         * 
-         * @param x The x coordinate of the waypoint.
-         * @param y The y coordinate of the waypoint.
-         */
-        public void appendWayPoint(Node n) {
-                waypoints.add(n);
-        }
-
-        /**
-         * Add a waypoint to the beginning of the path.  
-         * 
-         * @param x The x coordinate of the waypoint.
-         * @param y The y coordinate of the waypoint.
-         */
-        public void prependWayPoint(Node n) {
-                waypoints.add(0, n);
-        }
-
-        /**
-         * Check if this path contains the WayPoint
-         * 
-         * @param x The x coordinate of the waypoint.
-         * @param y The y coordinate of the waypoint.
-         * @return True if the path contains the waypoint.
-         */
-        public boolean contains(int x, int y) {
-                for(Node node : waypoints) {
-                        if (node.x == x && node.y == y)
-                                return true;
-                }
-                return false;
-        }
-        
-        public void printPath() {
-        	Node n, prev_n;
-        	int i = 0;
-        	
-	        System.out.println("Nodes on path:");
-	        n = waypoints.get(i);
-	        System.out.println("Path "+n.nodeNum+" ("+n.x+" , "+n.y+")");
-	        for(i=1; i<waypoints.size(); i++) {
-	        	prev_n = n;
-	        	n = waypoints.get(i);
-	        	System.out.println("Path "+n.nodeNum+" ("+n.x+" , "+n.y+")");
-	        	//mc.drawLine(prev_n.getX(), prev_n.getY(), n.getX(), n.getY());
-	        }
-
-        }
+    }
 
 }
